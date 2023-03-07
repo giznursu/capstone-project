2d772b07c30e64015857699a21bee4435b17d9b7
Gizem Cetinkaya <gizemce00@gmail.com>
2023-03-06 19:48:59 -0500

    data added

---
+++ data.js
@@ -0,0 +1,214 @@
+const products = [
+    {
+        category : "Bronzer",
+        brand : "DIOR",
+        name: "FOREVER NATURAL BRONZE",
+        color: "01"
+    },
+    {
+        category : "Bronzer",
+        brand : "DIOR",
+        name: "FOREVER NATURAL BRONZE",
+        color: "02"
+    },
+    {
+        category : "Bronzer",
+        brand : "DIOR",
+        name: "FOREVER NATURAL BRONZE",
+        color: "03"
+    },
+    {
+        category : "Bronzer",
+        brand : "DIOR",
+        name: "FOREVER NATURAL BRONZE",
+        color: "04"
+    },
+    {
+        category : "Bronzer",
+        brand : "DIOR",
+        name: "FOREVER NATURAL BRONZE",
+        color: "05"
+    },
+    {
+        category : "Bronzer",
+        brand : "FENTY BEAUTY",
+        name: "Sun Stalk'r Instant Warmth Bronzer",
+        color: "Inda Sun"
+    },
+    {
+        category : "Bronzer",
+        brand : "FENTY BEAUTY",
+        name: "Sun Stalk'r Instant Warmth Bronzer",
+        color: "Shady Biz"
+    },
+    {
+        category : "Bronzer",
+        brand : "FENTY BEAUTY",
+        name: "Sun Stalk'r Instant Warmth Bronzer",
+        color: "Caramel Cutie"
+    },
+    {
+        category : "Concelear",
+        brand : "DIOR",
+        name: "BACKSTAGE FLASH PERFECTOR CONCEALAR",
+        color: "0CR Cool Rosy"
+    },
+    {
+        category : "Concelear",
+        brand : "DIOR",
+        name: "BACKSTAGE FLASH PERFECTOR CONCEALAR",
+        color: "0W Warm"
+    },
+    {
+        category : "Concelear",
+        brand : "DIOR",
+        name: "BACKSTAGE FLASH PERFECTOR CONCEALAR",
+        color: "4W Warm"
+    },
+    {
+        category : "Concelear",
+        brand : "DIOR",
+        name: "BACKSTAGE FLASH PERFECTOR CONCEALAR",
+        color: "4C Cool"
+    },
+    {
+        category : "Concelear",
+        brand : "DIOR",
+        name: "BACKSTAGE FLASH PERFECTOR CONCEALAR",
+        color: "3N Neutral"
+    },
+    {
+        category : "Concelear",
+        brand : "TARTE",
+        name: "Shape Tape",
+        color: "60H Mahogany Honey"
+    },
+    {
+        category : "Concelear",
+        brand : "TARTE",
+        name: "Shape Tape",
+        color: "53N Deep"
+    },
+    {
+        category : "Concelear",
+        brand : "TARTE",
+        name: "Shape Tape",
+        color: "44H Tan"
+    },
+    {
+        category : "Concelear",
+        brand : "TARTE",
+        name: "Shape Tape",
+        color: "36S Medium-Tan Sand"
+    },
+    {
+        category : "Concelear",
+        brand : "TARTE",
+        name: "Shape Tape",
+        color: "22N Light Neutral"
+    },
+    {
+        category : "Lipgloss",
+        brand : "HERMES",
+        name: "Shape TapeHermesistible, Infused Care Oil",
+        color: "01-Beige Sapotille"
+    },
+    {
+        category : "Lipgloss",
+        brand : "HERMES",
+        name: "Shape TapeHermesistible, Infused Care Oil",
+        color: "02-Corail Bigarade"
+    },
+    {
+        category : "Lipgloss",
+        brand : "HERMES",
+        name: "Shape TapeHermesistible, Infused Care Oil",
+        color: "03-Rose Pitaya"
+    },
+    {
+        category : "Lipgloss",
+        brand : "HERMES",
+        name: "Shape TapeHermesistible, Infused Care Oil",
+        color: "04-Rouge Amarelle"
+    },
+    {
+        category : "Lipgloss",
+        brand : "HERMES",
+        name: "Shape TapeHermesistible, Infused Care Oil",
+        color: "06-Pourpre Camarine"
+    },
+    {
+        category : "Lipgloss",
+        brand : "YSL",
+        name: "ROUGE VOLUPTÉ SHINE LIPSTICK BALM",
+        color: "09 Nude Sheer"
+    },
+    {
+        category : "Lipgloss",
+        brand : "YSL",
+        name: "ROUGE VOLUPTÉ SHINE LIPSTICK BALM",
+        color: "12 Corail Dolman"
+    },
+    {
+        category : "Lipgloss",
+        brand : "YSL",
+        name: "ROUGE VOLUPTÉ SHINE LIPSTICK BALM",
+        color: "15 Corail Spontini"
+    },
+    {
+        category : "Lipgloss",
+        brand : "YSL",
+        name: "ROUGE VOLUPTÉ SHINE LIPSTICK BALM",
+        color: "16 Orange Majorelle"
+    },
+    {
+        category : "Lipgloss",
+        brand : "YSL",
+        name: "ROUGE VOLUPTÉ SHINE LIPSTICK BALM",
+        color: "44 Nude Lavalliere"
+    },
+    {
+        category : "Mascara",
+        brand : "YSL",
+        name: "LASH CLASH EXTREME VOLUME MASCARA",
+        color: "1 Black"
+    },
+    {
+        category : "Mascara",
+        brand : "YSL",
+        name: "LASH CLASH EXTREME VOLUME MASCARA",
+        color: "2 Brown"
+    },
+    {
+        category : "Mascara",
+        brand : "DIOR",
+        name: "DIORSHOW",
+        color: "090 Black"
+    },
+    {
+        category : "Mascara",
+        brand : "DIOR",
+        name: "DIORSHOW",
+        color: "288 Blue"
+    },
+    {
+        category : "Mascara",
+        brand : "DIOR",
+        name: "DIORSHOW",
+        color: "798 Brown"
+    },
+    {
+        category : "Mascara",
+        brand : "ESTEELAUDER",
+        name: "Double Wear Zero-Smudge Lengthening Mascara",
+        color: "Black"
+    },
+    {
+        category : "Mascara",
+        brand : "LANCOME",
+        name: "LASH IDÔLE MASCARA",
+        color: "Glossy Black"
+    },
+    
+    
+]
